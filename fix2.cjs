03290be1bb75f8a6f4d58716f6c4d5dcf02f3c05
Aryan <aryansaha7914@gmail.com>
2026-09-08 22:12:17 +0530

    removed unwanted stuffs

--- fix2.cjs
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-let c = fs.readFileSync('src/data/questions.js', 'utf8');
-c = c.replace(/\\`/g, '`');
-fs.writeFileSync('src/data/questions.js', c);
-console.log('Fixed backticks');
